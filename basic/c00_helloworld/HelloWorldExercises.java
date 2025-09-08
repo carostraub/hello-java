6e3ec7ef841d09fefca1b6809eb03b09d0d9c38e
caro.straub <carolina.straub@gmail.com>
2025-09-08 20:02:27 -0300

    lo que he ido aprendiendo de Java

--- basic/c00_helloworld/HelloWorldExercises.java
+++ basic/c00_helloworld/HelloWorldExercises.java
@@ -1,13 +1,25 @@
-package basic.c00_helloworld;
+package basic.c00_helloworld; //Funciona como una carpeta que organiza tu código
 
 /*
 Clase 14 - Ejercicios: Primeros pasos
 Vídeo: https://youtu.be/JOAqpdM36wI?t=5108
 */
+/*
+Trabajo en la pega que quiero
+Con un buen ambiente laboral
+Y buen sueldo
+Soy feliz
+ */
+public class HelloWorldExercises { // la clase principal
 
-public class HelloWorldExercises {
-
-    public static void main(String[] args) {
+    public static void main(String[] args) { // Es lo que hace que el sistema se ejecute
+        System.out.println("Hola\nMundo"); // Imprime el Hola mundo
+        System.out.println("28, morado,rosado, celeste y verde claro, Santiago");
+        System.err.println("a ver que sale");
+        System.out.println("Trabajo");
+        System.out.println("en la");
+        System.out.println("misma empresa");
+        System.out.println("que mi hermano");
 
         // 1. Imprime un mensaje que diga tu nombre en lugar de "¡Hola Mundo!".
 
@@ -24,6 +36,11 @@ public class HelloWorldExercises {
         // 7. Utiliza varios println para imprimir una frase.
 
         // 8. Imprime un diseño ASCII (por ejemplo, una cara feliz usando símbolos).
+        System.out.println("   *   ");
+        System.out.println("  ***  ");
+        System.out.println(" ***** ");
+        System.out.println("*******");
+        System.out.println("   |   ");
 
         // 9. Intenta ejecutar el programa sin el método main y observa el error.
 
--- basic/c01_beginner/BeginnerExercises.java
+++ basic/c01_beginner/BeginnerExercises.java
@@ -10,23 +10,49 @@ public class BeginnerExercises {
     public static void main(String[] args) {
 
         // 1. Declara una variable de tipo String y asígnale tu nombre.
+        String name = "Caro";
+        System.out.println(name);
 
         // 2. Crea una variable de tipo int y asígnale tu edad.
+        int age = 28;
+        System.out.println(age);
 
         // 3. Crea una variable double con tu altura en metros.
+        double height = 1.59; //en verdad ni idea cuanto mido lol
+        System.out.println(height);
 
         // 4. Declara una variable de tipo boolean que indique si te gusta programar.
+        boolean likeProgramming = true;
+        System.out.println(likeProgramming);
 
         // 5. Declara una constante con tu email.
+        String email = "carolina.straub@gmail.com";
+        System.out.println(email);
 
         // 6. Crea una variable de tipo char y guárdale tu inicial.
+        char initial = 'C';
+        System.out.println(initial);
 
         // 7. Declara una variable de tipo String con tu localidad, y a continuación cambia su valor y vuelve a imprimirla.
+        String location = "Chile";
+        System.out.println(location);
+        location = "Santiago";
+        System.out.println(location);
 
         // 8. Crea una variable int llamada a, otra b, e imprime la suma de ambas.
+        int a = 4;
+        int b = 6;
+        System.out.println(a+b);
 
         // 9. Imprime el tipo de dos variables creadas anteriormente.
 
+        System.out.println(((Object)age).getClass().getSimpleName());
+        System.out.println(((Object)height).getClass().getSimpleName());
+
         // 10. Intenta declarar una variable sin inicializarla y luego asígnale un valor antes de imprimirla.
+        int noValue;
+        noValue = 5;
+        System.out.println(noValue);
+
     }
 }
